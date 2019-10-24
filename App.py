e061581136fcc24118f61e484aa7195c04fe64cd
Subham Raj <subhamraj4114@gmail.com>
2019-10-24 13:25:29 +0530

    added comments to app.py

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
-# GUI: 
-import tkinter as tk
-import GetIcon
-from tkinter import font
-import requests
-from PIL import Image, ImageTk
+import tkinter as tk # tkinter in Python's native GUI library
+import GetIcon # importing and executing the GetIcon.py file's code
+from tkinter import font # tkinter's libarary for fonts 
+import requests # pip's Requests package to talk to API's
+from PIL import Image, ImageTk # pip Pillow libarary to display the weather icons
 
+# setting default values
 HEIGHT = 400
 WIDTH = 650
 PRIMARYBG = '#333'
@@ -13,10 +13,12 @@ TERNARYBG = '#777'
 FONT = 'Courier'
 PLACEHOLDER = 'enter the city...'
 
+# function to remove text presnet in the entry field
 def userText(event):
     entry.delete(0,len(PLACEHOLDER))
     usercheck=True
 
+# function to display weather icon
 def AddIcon(icon):
     size = int(lowerFrame.winfo_height()*0.25)
     img = ImageTk.PhotoImage(Image.open('./img/'+icon+'.png').resize((size, size)))
@@ -24,8 +26,8 @@ def AddIcon(icon):
     weatherIcon.create_image(0,0, anchor='nw', image=img)
     weatherIcon.image = img
 
-
-def FormateWeather(weather):
+# function to format and display the json data
+def FormatWeather(weather):
     try:
         city = weather['name']
         desc = weather['weather'][0]['description']
@@ -37,6 +39,7 @@ def FormateWeather(weather):
     except:
         return 'Sorry! The was a problem retrieving information.'
 
+# function to make API call to openweathermap.org and parse the result
 def GetWeather(city):
     try:
         if(city in [PLACEHOLDER, '']):
@@ -47,38 +50,45 @@ def GetWeather(city):
             params = {'APPID': weatherKey, 'q':city, 'units': 'imperial'}
             response = requests.get(url, params=params)
             weather = response.json()
-            label['text'] = FormateWeather(weather)
+            label['text'] = FormatWeather(weather)
     except:
         label['text'] = 'RIP!'
 
+# root
 root = tk.Tk()
 root.minsize(height=HEIGHT, width=WIDTH)
 root.maxsize(height=HEIGHT+100, width=WIDTH+200)
 root.title("Dark Weather")
 root.iconbitmap(r'./assests/sun_icon.ico')
 
+# canvas
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg=PRIMARYBG)
 canvas.pack(fill='both', expand=True)
 
+# upper frame housing entry and button
 upperFrame = tk.Frame(root, bg=SECONDARYBG, bd=5)
 upperFrame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
+# entry widget to enter city name
 entry = tk.Entry(upperFrame, bg=TERNARYBG, font=(FONT, 12), borderwidth=5, relief=tk.FLAT)
 entry.place(relwidth=0.65, relheight=1)
 entry.insert(0, PLACEHOLDER)
 entry.bind("<Button>",userText)
 
+# button widget to call actions
 button = tk.Button(upperFrame, text="Get Weather", bg=TERNARYBG, font=(FONT, 12), command=lambda: GetWeather(entry.get()))
 button.place(relx=0.7, relwidth=0.3, relheight=1)
-# root.bind('<Return>', lambda event, x=entry.get(): GetWeather(x))
 
+# lower frame housing label and weahterIcon
 lowerFrame = tk.Frame(root, bg=SECONDARYBG, bd=5)
 lowerFrame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
+# label widget to display all the parsed weather data
 label = tk.Label(lowerFrame, text='...', bg=TERNARYBG, font=(FONT, 12), bd=5)
 label.place(relwidth=1, relheight=1)
 
+# canvas used to display weather icon
 weatherIcon = tk.Canvas(lowerFrame, bg=TERNARYBG, bd=5, highlightthickness=0)
 weatherIcon.place(relx=.85, rely=0.01, relwidth=0.15, relheight=0.3)
 
-root.mainloop()
+root.mainloop() # runs the application
